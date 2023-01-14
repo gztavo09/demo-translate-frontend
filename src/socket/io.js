be42135db6fd401e3a41c4001dc271d669bf573f
Gustavo Arias <gjarias@arqsoft.com.pe>
2023-01-14 03:00:32 -0500

    socket

--- src/socket/io.js
+++ src/socket/io.js
@@ -1,5 +1,5 @@
 import io from 'socket.io-client';
 
-const socket = io('https://dashing-custard-186365.netlify.app:3000/bundle.js');
+const socket = io('https://dashing-custard-186365.netlify.app:3000');
 
 export default socket;
